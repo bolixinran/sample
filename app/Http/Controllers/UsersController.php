0f0e75f442513934c209c5235dca1541eb608765
bolixinran <1325374347@qq.com>
2017-06-13 18:48:05 +0800

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,14 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class UsersController extends Controller
 {
-    public function create()
+  public function create()
+  {
+      return view('users.create');
+  }
+
+  public function show($id)
+  {
+      $user = User::findOrFail($id);
+      return view('users.show', compact('user'));
+  }
+
+  public function store(Request $request)
     {
-        return view('users.create');
+      $this->validate($request, [
+          'name' => 'required|max:50',
+          'email' => 'required|email|unique:users|max:255',
+          'password' => 'required|confirmed|min:6'
+      ]);
+
+      $user = User::create([//用户模型 User::create() 创建成功后会返回一个用户对象，并包含新注册用户的所有信息。我们将新注册用户的所有信息赋值给变量 $user，并通过路由跳转来进行数据绑定。
+          'name' => $request->name,
+          'email' => $request->email,
+          'password' => bcrypt($request->password),
+      ]);
+       session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+      return redirect()->route('users.show', [$user]);//后面那个是路由参数
     }
 }
