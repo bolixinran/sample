c03e1f64ec1fa3bbaf856ffeb6d829f24f7fa9b5
bolixinran <1325374347@qq.com>
2017-06-23 19:26:36 +0800

    Add password resets & email configuration

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -20,13 +20,15 @@ class PasswordController extends Controller
 
     use ResetsPasswords;
 
+     protected $redirectPath = '/';
+
     /**
      * Create a new password controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
+     public function __construct()
+     {
+         $this->middleware('guest');
+     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,7 +26,17 @@ Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
+
+
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');//激活账号路由
+
+
+
+//以下是密码重置路由
+Route::get('password/email', 'Auth\PasswordController@getEmail')->name('password.reset');
+Route::post('password/email', 'Auth\PasswordController@postEmail')->name('password.reset');
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset')->name('password.edit');
+Route::post('password/reset', 'Auth\PasswordController@postReset')->name('password.update');
 
 // Route::resource('users', 'UsersController');  这一行等价于下面的7条
 
